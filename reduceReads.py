279467abe76b1099592ef3561ebed90c54b3353c
mjsull <mjsull@gmail.com>
2013-05-09 11:48:12 +1000

    firstCommit

---
+++ reduceReads.py
@@ -0,0 +1,34 @@
+import sys, random
+
+if sys.argv[4][-1] == 'z':
+    infile = gzip.open(sys.argv[1])
+    outfile = gzip.open(sys.argv[2], 'wb')
+else:
+    infile = open(sys.argv[1])
+    outfile = open(sys.argv[2], 'w')
+if sys.argv[4][:2] == 'fa':
+    z = 2
+else:
+    z = 4
+getrate = float(sys.argv[3])
+
+
+
+count = 0
+for line in infile:
+    count += 1
+    if count % z == 1:
+        if random.random <= getrate:
+            getit = True
+            outfile.write(line)
+        else:
+            getit = False
+    elif count % z == 2:
+        if getit:
+            outfile.write(line)
+    elif count % z == 3:
+        if getit:
+            outfile.write(line)
+    elif count % z == 0:
+        if getit:
+            outfile.write(line)
